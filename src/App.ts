f210117495c024f4e1fdff423ea7f75983c311f4
unknown <jec.dev.22@gmail.com>
2023-10-25 19:49:18 -0300

    Cambiar el export default de email.router.ts

--- src/App.ts
+++ src/App.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import './config/database'
 import express from 'express'
 import cors from 'cors'
-import emailRouter from './routers/email.router'
+import { emailRouter } from './routers/email.router'
 import { projectRouter } from './routers/project.router'
 import { skillRouter } from './routers/skill.router'
 
@@ -16,7 +16,7 @@ app.use(cors({
 
 app.use(express.json())
 
-app.use('/api', emailRouter)
+app.use('/api/email', emailRouter)
 app.use('/api/project', projectRouter)
 app.use('/api/skill', skillRouter)
 
--- src/routers/email.router.ts
+++ src/routers/email.router.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express'
 import { postEmail } from '../controllers/email.controller'
 
-const emailRouter = Router()
+export const emailRouter = Router()
 
-emailRouter.post('/email', (req, res) => postEmail(req, res))
-
-export default emailRouter
+emailRouter.post('/', (req, res) => postEmail(req, res))

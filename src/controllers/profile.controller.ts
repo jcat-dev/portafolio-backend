3fc92e2599dc0c605d045330c544e7a2400b04d6
unknown <jec.dev.22@gmail.com>
2024-04-10 15:27:54 -0300

    Eliminar getProfileById y putProfileById en la ruta /api/profile

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,19 +1,11 @@
 import { Request, Response } from 'express'
-import { findData, findDataById, replaceFirstData, updateDataById } from './crud.controller'
+import { findData, replaceFirstData } from './crud.controller'
 import { ProfileModel } from '../models/profile.model'
 
 export const getProfiles = (res: Response) => {
   return findData(ProfileModel, res)
 }
 
-export const getProfileById = (req: Request, res: Response) => {
-  return findDataById(req.params.id, ProfileModel, res)
-}
-
 export const postProfile = (req: Request, res: Response) => {
   return replaceFirstData(res, ProfileModel, req.body)
-}
-
-export const putProfileById = (req: Request, res: Response) => {
-  return updateDataById(req.params.id, req.body, ProfileModel, res)
 }
--- src/routers/profile.router.ts
+++ src/routers/profile.router.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express'
-import { getProfileById, getProfiles, postProfile, putProfileById } from '../controllers/profile.controller'
+import { getProfiles, postProfile } from '../controllers/profile.controller'
 
 export const profileRouter = Router()
 
 profileRouter.get('/' , (_, res) => getProfiles(res))
-profileRouter.get('/:id', (req, res) => getProfileById(req, res))
 profileRouter.post('/' , (req, res) => postProfile(req, res))
-profileRouter.put('/:id' , (req, res) => putProfileById(req, res))

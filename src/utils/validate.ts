7c5d031a83a51b0c5b49d9d3a5dd6a3bf988bf17
unknown <jec.dev.22@gmail.com>
2024-04-30 22:50:58 -0300

    Agregar la fn validateEMAIL en /utils/validate.ts

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -6,3 +6,15 @@
 export const validateURL = (url: string) => {
   return url.startsWith('https://') || url.startsWith('http://')
 }
+
+/**
+ * true if email is correct
+ * @param email 
+ * @returns 
+ */
+export const validateEMAIL = (email: string) => {
+  const firstIndex = email.indexOf('@')
+  const lastIndex = email.lastIndexOf('@')
+
+  return firstIndex > 0 && lastIndex < (email.length - 1)
+}

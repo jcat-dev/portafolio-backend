2012e1787cd8842583528aabc08bb4f2dc9a4cb3
unknown <jec.dev.22@gmail.com>
2024-05-21 23:45:27 -0300

    Cambiar los valores de skills de toLowerCase a ‘Capitalize’ en fn formatSkill de /middleware/skill.middleware.ts

--- src/middleware/skill.middleware.ts
+++ src/middleware/skill.middleware.ts
@@ -16,6 +16,6 @@ export const validateSkill = (req: Request, res: Response, next: NextFunction) =
 export const formatSkill = (req: Request, res: Response, next: NextFunction) => {
   const skill = req.body as Skill
   skill.title = skill.title.toUpperCase()
-  skill.skills = skill.skills.map((value) => value.toLowerCase())
+  skill.skills = skill.skills.map((value) => value.charAt(0).toUpperCase().concat(value.slice(1)))
   next()
 }
